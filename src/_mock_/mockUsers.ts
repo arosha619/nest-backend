13f1e9d8119f04fb46be6b544be9d31b0057b53c
arosha619 <sandaruwanmdgar.19@itfac.mrt.ac.lk>
2025-11-02 16:59:16 +0530

    quries

---
+++ src/_mock_/mockUsers.ts
@@ -0,0 +1,17 @@
+export const mockUsers = [
+    {
+        id: 1,
+        user_name: "user name example",
+        display_name: "display name example",
+    },
+    {
+        id: 2,
+        user_name: "user name example 2",
+        display_name: "display name example 2",
+    },
+    {
+        id: 3,
+        user_name: "user name example 3",
+        display_name: "display name example 3",
+    },
+]
--- src/graphql/resolvers/UserResolver.ts
+++ src/graphql/resolvers/UserResolver.ts
@@ -1,15 +1,16 @@
-import { Query, Resolver } from "@nestjs/graphql";
+import { Args, Query, Resolver, Int } from "@nestjs/graphql";
 import { User } from "../models/User";
+import { mockUsers } from "src/_mock_/mockUsers";
 
 @Resolver()
 export class UserResolver {
-    @Query((returns) => User)
-    getUser(){
-        return {
-            id: 1,
-            user_name: "user name example",
-            display_name: "display name example",
-        }
+    @Query((returns) => User, { nullable: true })
+    getUsersByID(@Args('id', { type: () => Int }) id: number){
+        return mockUsers.find((user) => user.id === id);
     }
 
+    @Query((returns) => [User], { nullable: 'items' })
+    getUsers(){
+        return mockUsers;
+    }
 }
